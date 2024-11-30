9ecd5ab2363160bbad7878e2d08a4fad6fcce1a4
Julie Cheng <jucheng925@gmail.com>
2024-11-30 02:33:51 -0500

    rover selection tab added additional styling

--- src/components/Rover/Rover.jsx
+++ src/components/Rover/Rover.jsx
@@ -2,57 +2,58 @@ import { useState } from 'react'
 import RoverInfo from './RoverInfo'
 
 const Rover = () => {
-  const [selectedRover, setSelectedRover] = useState("")
-  const [openInfo, setOpenInfo] = useState(false)
-
-
+  const [selectedRover, setSelectedRover] = useState("perseverance")
 
   return (
-    <div className="w-full max-w-7xl px-6 py-6 bg-gray-800 rounded-3xl shadow-lg ">
-      <h2 className="text-3xl font-bold">Mars Rovers</h2>
-      <p className='py-2'>Mars rovers are robotic vehicles designed to explore the surface of Mars. They capture high-resolution images, analyze soil and rock samples, and collect data to study the planet's geology, climate, and potential for past or present life.</p>
-      <p className='pb-2 italic'>Click to learn more about a specific rover and view the images it captured of Mars.</p>
+    <div className="w-full max-w-7xl px-8 py-6 bg-gray-900 rounded-3xl shadow-lg ">
+      <div className="self-center text-center">
+        <h2 className="text-3xl font-bold">Mars Rover</h2>
+        <hr className="border-b-teal-400 border-b-2 border-t-0 w-32 mx-auto my-4"></hr>
+        <p className="text-gray-400 pb-4">
+          UNMANNED VEHICLES MAPPING DISTANT PLACES
+        </p>
+      </div>
+
+      <div className='container mx-auto px-4 md:px-32 xl:px-64'>
+        <p className='py-2'>Mars rovers are robotic vehicles designed to explore the surface of Mars. They capture high-resolution images, analyze soil and rock samples, and collect data to study the planet's geology, climate, and potential for past or present life.</p>
+        <p className='pb-2 italic'>Click to learn more about a specific rover and view the images it had captured of Mars.</p>
+      </div>
       <div className='flex'>
         <div className="flex flex-col">
           <button 
-              className="bg-gray-300 hover:bg-gray-400 text-gray-500 font-bold py-2 px-4 rounded-l"
-              onClick={()=>{
-                setSelectedRover("perseverance")
-                setOpenInfo(true)
-              }}
+            className="bg-teal-200 hover:bg-teal-500 text-slate-600 font-bold py-2 px-4 border-y-2 border-y-gray-900 rounded-l-lg -mt-0.5"
+            onClick={()=>{
+              setSelectedRover("perseverance")
+            }}
           >
             Perseverance
           </button>
           <button 
-            className="bg-gray-300 hover:bg-gray-400 text-gray-500 font-bold py-2 px-4"
+            className="bg-teal-200 hover:bg-teal-500 text-slate-600 font-bold py-2 px-4 border-y-2 border-y-gray-900 rounded-l-lg"
             onClick={()=>{
               setSelectedRover("curiosity")
-              setOpenInfo(true)
             }}
           >
             Curiosity
           </button>
           <button 
-            className="bg-gray-300 hover:bg-gray-400 text-gray-500 font-bold py-2 px-4"
+            className="bg-teal-200 hover:bg-teal-500 text-slate-600 font-bold py-2 px-4 border-y-2 border-y-gray-900 rounded-l-lg"
             onClick={()=>{
               setSelectedRover("opportunity")
-              setOpenInfo(true)
             }}
           >
             Opportunity
           </button>
           <button 
-            className="bg-gray-300 hover:bg-gray-400 text-gray-500 font-bold py-2 px-4 rounded-r"
+            className="bg-teal-200 hover:bg-teal-500 text-slate-600 font-bold py-2 px-4 border-y-2 border-y-gray-900 rounded-l-lg"
             onClick={()=>{
               setSelectedRover("spirit")
-              setOpenInfo(true)
             }}
           >
             Spirit
           </button>
         </div>
         <RoverInfo selectedRover={selectedRover}/>
-        {/* {openInfo && <RoverInfo selectedRover={selectedRover}/>} */}
       </div>
     </div>
   )

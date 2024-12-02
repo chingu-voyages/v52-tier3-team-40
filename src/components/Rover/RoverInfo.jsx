35bce13af8a1e1cf4603f7220bf6e262f19dc318
Julie Cheng <jucheng925@gmail.com>
2024-12-02 06:42:48 -0500

    more styling changes

--- src/components/Rover/RoverInfo.jsx
+++ src/components/Rover/RoverInfo.jsx
@@ -59,111 +59,94 @@ const RoverInfo = ({selectedRover}) => {
 			{!roverInfo ? 
 				<p className="font-bold text-xl">Loading...</p> 
 					: 
-				<div className='flex flex-col sm:flex-row sm:gap-6'>
-					<div className='w-full sm:w-1/2 mt-2 sm:mt-5 '>
-						<img
-							src={roverImages[selectedRover]}
-							alt="Rover"
-							class="w-full h-auto object-cover"
-						/>
-					</div>
+				<div className='container'>
+
+					<div className='flex flex-col sm:flex-row sm:gap-6'>
+						<div className='w-full sm:w-1/2 mt-2 sm:mt-5 '>
+							<img
+								src={roverImages[selectedRover]}
+								alt="Rover"
+								className="w-full h-auto object-cover"
+							/>
+						</div>
+
 						<div className="w-full sm:w-1/2">
 							<dl className="divide-y divide-gray-100">
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Rover Name</dt>
 									<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">{roverInfo.name}</dd>
 								</div>
-								
+									
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Rover Status</dt>
 									<dd className={`mt-1 text-sm/6 sm:mt-0 ${roverInfo.status === "complete" ? 'text-red-300' : 'text-emerald-300'}`}>{roverInfo.status.toUpperCase()}</dd>
 								</div>
-											
+												
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Launch Date</dt>
 									<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">{roverInfo.launch_date}</dd>
 								</div>
-											
+												
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Landing Date</dt>
 									<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">{roverInfo.landing_date}</dd>
 								</div>
-											
+												
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Sols on Mission (Martian Days)</dt>
 									<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">{roverInfo.max_sol}</dd>
 								</div>
-														
+															
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
 									<dt className="font-black text-base text-gray-100">Total Photos Taken</dt>
 									<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">{roverInfo.total_photos}</dd>
 								</div>
 
 								<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
-								</div>
-								
+								</div>		
 							</dl>
-											<div className="p-2 sm:p-3 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
-												<dt className="font-black text-base text-gray-100">Cameras</dt>
-												<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0">
-														{roverInfo.cameras.map((camera)=>
-																<button 
-																		className={`font-bold py-2 px-4 ${
-																				isSpiritOrOpportunity 
-																						? "text-gray-100 cursor-not-allowed" 
-																						: "hover:bg-gray-400 text-gray-100"
-																				} ${
-																				selectedCamera === camera.name 
-																						? "bg-red-500 hover:bg-red-500"
-																						: "bg-gray-300"	
-																				}`
-																		} 
-																		disabled={isSpiritOrOpportunity}
-																		key={camera.name}
-																		onClick={()=>setSelectedCamera(camera.name)}
-																>
-																		{camera.full_name}
-																</button>
-														)}
-														{isSpiritOrOpportunity && <p className='text-lg text-bold text-red-500'>Sorry, images from Spirit and Opportunity can not be fetched.</p>}
-												</dd>
-											</div>
+						</div>
+					</div>
+
+					<div className="p-2 sm:grid sm:grid-cols-7 sm:px-0">
+						<dt className="font-black text-base text-gray-100">Cameras 
+							<span className='text-xs inline-block font-thin italic'>Choose a camera to view the photos captured by the rover.</span>
+						</dt>
+						<dd className="mt-1 text-sm/6 text-gray-100 sm:mt-0 sm:col-span-6">
+							{roverInfo.cameras.map((camera)=>
+								<button 
+									className={`font-bold py-2 rounded-md m-1 px-4 ${
+										isSpiritOrOpportunity 
+											? "text-slate-600 cursor-not-allowed" 
+											: "hover:bg-teal-500 text-slate-600"
+										} ${
+										selectedCamera === camera.name 
+											? "bg-red-300"
+											: "bg-teal-200"	
+										}`
+									} 
+									disabled={isSpiritOrOpportunity}
+									key={camera.name}
+									onClick={()=>setSelectedCamera(camera.name)}
+								>
+									{camera.full_name}
+								</button>
+							)}
+							{isSpiritOrOpportunity && <p className='text-lg text-bold text-red-300'>Sorry, images from Spirit and Opportunity can not be fetched.</p>}
+						</dd>
+					</div>
 										
-														{selectedCamera && (
-																isFetchingPhotosComplete ? 
-																		<RoverPhotosDisplay 
-																				photos={photos}
-																				camera={selectedCamera}
-																		/>
-																:
-																		<p className="font-bold text-xl">Loading...</p> 
-														)}
-									</div>
-								</div>
-						}
-			{/* <div class="flex max-w-2xl bg-gray-50 border border-gray-200 rounded-lg shadow-md overflow-hidden">
-	<img
-		src="rover-image.jpg"
-		alt="Rover"
-		class="w-1/3 object-cover"
-	/>
-	<div class="p-4 w-2/3">
-		<h2 class="text-xl font-bold text-gray-800">Rover Name: Curiosity</h2>
-		<p class="text-gray-600 text-sm mt-2">
-			Launched: November 26, 2011<br />
-			Mission: Explore Gale Crater on Mars.
-		</p>
-		<p class="text-gray-700 mt-3">
-			Curiosity is equipped with a drill, cameras, and sensors to analyze soil and rocks.
-		</p>
-		<a
-			href="#"
-			class="text-teal-500 font-semibold mt-4 inline-block hover:underline"
-		>
-			Read More →
-		</a>
-	</div>
-</div> */}
+					{selectedCamera && (
+						isFetchingPhotosComplete ? 
+							<RoverPhotosDisplay 
+								photos={photos}
+								camera={selectedCamera}
+							/>
+						:
+							<p className="font-bold text-xl">Loading...</p> 
+						)}
+				</div>
+			}
 		</div>
 	)
 }
--- src/components/Rover/RoverPhotosDisplay.jsx
+++ src/components/Rover/RoverPhotosDisplay.jsx
@@ -2,9 +2,9 @@ import {useState, useRef} from 'react'
 
 const RoverPhotosDisplay = ({photos, camera}) => {
   const [selectedPhoto, setSelectedPhoto] = useState(null)
+  console.log(selectedPhoto)
 
   const filterPhotos = photos?.filter((photo)=> photo.camera.name === camera)
-  console.log(filterPhotos)
 
   const scrollContainerRef = useRef(null)
 
@@ -18,71 +18,71 @@ const RoverPhotosDisplay = ({photos, camera}) => {
 
   return (
     <>
-    {!filterPhotos.length ? 
-          <p className="font-bold text-xl">
-            Sorry there were no photos taken by {camera} in the last 5 sols.
-          </p>
-          :
-    <div className="relative w-full">
-      {/* Scroll Left Button */}
-      <button
-        className="absolute -left-4 top-1/2 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full shadow-md"
-        onClick={scrollLeft}
-      >
-        ←
-      </button>
-
-      {/* Scrollable Container */}
-      <div
-        ref={scrollContainerRef}
-        className="flex space-x-4 overflow-x-auto scrollbar-hide scroll-smooth w-full p-4"
-      >
-        {
-          filterPhotos.map(photo => (
-          <img
-            key={photo.id}
-            src={photo.img_src}
-            alt={photo.camera.full_name}
-            className="h-40 sm:h-48 md:h-64 rounded-lg shadow-md hover:scale-105 ease-in-out duration-300"
-            onClick={()=>setSelectedPhoto(photo)}
-          />
-        ))}
-      </div>
-
-      {/* Scroll Right Button */}
-      <button
-        className="absolute -right-4 top-1/2 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full shadow-md"
-        onClick={scrollRight}
-      >
-        →
-      </button>
-
-      {selectedPhoto && (
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center transition-opacity justify-center z-50">
-        <div className="bg-white p-6 md:p-10 rounded-lg max-w-3xl w-full relative">
+      {!filterPhotos.length ? 
+        <p className="font-bold text-xl">
+          Sorry there were no photos taken by {camera} in the last 5 sols.
+        </p>
+      :
+        <div className="relative w-full">
+      
+          {/* Scroll Left Button */}
           <button
-            className="absolute top-1 md:top-2 right-2 md:right-3 text-1xl md:text-2xl font-bold text-gray-500 hover:text-gray-900"
-            onClick={() => setSelectedPhoto(null)}
+            className="absolute -left-6 sm:-left-4 top-1/2 box-border transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 text-lg rounded-full shadow-md"
+            onClick={scrollLeft}
           >
-            ✕
+            ←
           </button>
-          <img
-            src={selectedPhoto.img_src}
-            alt={selectedPhoto.camera.full_name}
-            className="rounded-lg mb-4"
-          />
-          <div className="mx-48">
-            <h3 className="text-lg font-bold">{selectedPhoto.camera.full_name}</h3>
-            <p className="text-gray-600">Sol: {selectedPhoto.sol}</p>
-            <p className="text-gray-600">Earth Date: {selectedPhoto.earth_date}</p>
-            <p className="text-gray-600 ">Rover: {selectedPhoto.rover.name}</p>
+
+          {/* Scrollable Container */}
+          <div
+            ref={scrollContainerRef}
+            className="flex space-x-4 overflow-x-auto scrollbar-hide scroll-smooth w-full p-4"
+          >
+            {filterPhotos.map(photo => (
+              <img
+                key={photo.id}
+                src={photo.img_src}
+                alt={photo.camera.full_name}
+                className="h-40 sm:h-48 md:h-64 rounded-lg shadow-md hover:scale-105 ease-in-out duration-300"
+                onClick={()=>setSelectedPhoto(photo)}
+              />
+            ))}
           </div>
+
+          {/* Scroll Right Button */}
+          <button
+            className="absolute -right-6 sm:-right-4 top-1/2 transform -translate-y-1/2 z-10 box-border bg-gray-800 text-white text-lg p-2 rounded-full shadow-md"
+            onClick={scrollRight}
+          >
+            →
+          </button>
+
+          {selectedPhoto && (
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center transition-opacity justify-center z-50">
+              <div className="bg-white p-6 md:p-10 rounded-lg w-3/4 sm:w-3/5 relative">
+                <button
+                  className="absolute top-1 md:top-2 right-2 md:right-3 text-1xl md:text-2xl border-box font-bold text-gray-500 hover:text-gray-900"
+                  onClick={() => setSelectedPhoto(null)}
+                >
+                  ✕
+                </button>
+                <img
+                  src={selectedPhoto.img_src}
+                  alt={selectedPhoto.camera.full_name}
+                  className="rounded-lg mb-4"
+                />
+                <div className="w-3/5 m-auto">
+                  <h3 className="text-lg text-center font-bold text-slate-900">{selectedPhoto.camera.full_name}</h3>
+                  <p className="text-gray-700 text-center">Sol: {selectedPhoto.sol}</p>
+                  <p className="text-gray-700 text-center">Earth Date: {selectedPhoto.earth_date}</p>
+                  <p className="text-gray-700 text-center">Rover: {selectedPhoto.rover.name}</p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-    )}
-    </div>
-  }
-  </>
+      }
+    </>
   );
 };
 

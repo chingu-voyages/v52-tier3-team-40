0fb50d08a425c942aa0455de62ace8ecc9eede00
Julie Cheng <jucheng925@gmail.com>
2024-12-03 03:55:49 -0500

    adjust tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -27,7 +29,5 @@ export default {
     }
   },
   darkMode: "class",
-  plugins: [
-    require("tailwind-scrollbar-hide")
-  ],
+  plugins: [ tailwindScrollbarHide ],
 };

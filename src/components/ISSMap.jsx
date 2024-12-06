9c7703e31d6c1a9f77a476ccceed26eab58db551
ss-deep <deepalisangole@gmail.com>
2024-12-05 19:33:42 -0600

    Added ISS tracker in component 2

--- src/components/ISSMap.jsx
+++ src/components/ISSMap.jsx
@@ -1,36 +1,19 @@
-// import React from "react";
-
-// const Component2 = () => (
-//   <div className="w-full max-w-7xl h-[60vh] px-4 py-6 bg-gray-800 rounded-3xl shadow-lg">
-//     <h2 className="text-lg font-semibold">Component 2</h2>
-//     <p>This is the second component of the homepage.</p>
-//   </div>
-// );
-
-// export default Component2;
-
 import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet's CSS
-
+import 'leaflet/dist/leaflet.css';
 const ISSMap = () => {
-  const mapRef = useRef(null); // Reference to store the Leaflet map instance
-  const issMarkerRef = useRef(null); // Reference for the ISS marker
-  const issCircleRef = useRef(null); // Reference for the ISS circle
-  const [position, setPosition] = useState({ lat: 0, lon: 0 }); // Store ISS position
-
-  // Initialize the map and layers
+  const mapRef = useRef(null);
+  const issMarkerRef = useRef(null);
+  const issCircleRef = useRef(null);
+  const [position, setPosition] = useState({ lat: 0, lon: 0 });
   useEffect(() => {
-    // Create the map instance and set initial view
     mapRef.current = L.map('map').setView([0, 0], 2);
 
-    // Add tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 4,
       minZoom: 1,
     }).addTo(mapRef.current);
 
-    // Define the ISS icon
     const ISSIcon = L.icon({
       iconUrl: '/src/assets/images/ISS/ISSIcon.png',
       iconSize: [50, 30],
@@ -41,12 +24,10 @@ const ISSMap = () => {
       shadowAnchor: [30, 15],
     });
 
-    // Add ISS marker to the map
     issMarkerRef.current = L.marker([0, 0], { icon: ISSIcon }).addTo(
       mapRef.current
     );
 
-    // Add ISS circle to the map
     issCircleRef.current = L.circle([0, 0], {
       radius: 2200e3,
       color: '#c22',
@@ -56,7 +37,6 @@ const ISSMap = () => {
       fillOpacity: 0.1,
     }).addTo(mapRef.current);
 
-    // Cleanup the map on component unmount
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
@@ -64,7 +44,6 @@ const ISSMap = () => {
     };
   }, []);
 
-  // Fetch ISS position and update map layers
   useEffect(() => {
     const fetchISSPosition = () => {
       fetch('http://api.open-notify.org/iss-now.json')
@@ -72,7 +51,7 @@ const ISSMap = () => {
         .then((data) => {
           const lat = parseFloat(data.iss_position.latitude);
           const lon = parseFloat(data.iss_position.longitude);
-          setPosition({ lat, lon }); // Update state with the new position
+          setPosition({ lat, lon });
         })
         .catch((error) => console.error('Error fetching ISS data:', error));
     };

719b1335d252e0c021a7361566dc5773e9e9bace
Julie Cheng <jucheng925@gmail.com>
2024-12-08 16:30:09 -0500

    refactor so all components has same color background

--- src/components/SlidingPuzzle/SlidingPuzzle.jsx
+++ src/components/SlidingPuzzle/SlidingPuzzle.jsx
@@ -5,7 +5,7 @@ const SlidingPuzzle = () => {
   const [isStarted, setIsStarted] = useState(false);
 
   return (
-    <div className="text-center space-y-4">
+    <div className="text-center space-y-4 w-full max-w-7xl px-8 pt-10 pb-20 bg-gray-900 rounded-3xl shadow-lg">
       <h2 className="text-3xl font-bold">Sliding Puzzle</h2>
       <hr className="border-b-teal-400 border-b-2 border-t-0 w-32 mx-auto my-4"></hr>
       <Board />
